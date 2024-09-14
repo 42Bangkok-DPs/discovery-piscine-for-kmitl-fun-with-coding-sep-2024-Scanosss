329540ab04bd55b79a8dbfb5de7768e570524412
Scanos <97615333+Scanosss@users.noreply.github.com>
2024-09-14 16:02:46 +0700

    Add files via upload

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,48 +1,36 @@
 $(document).ready(function() {
-  // Load TODO from cookies when the page loads
-  loadTodosFromCookie()
+    loadTasks();
 
-  // create a new TODO
-  $('#newTodo').click(function() {
-    const todoText = prompt('Enter a new TO DO:')
-    if (todoText && todoText.trim() !== '') {
-      addTodo(todoText)
-      saveTodosToCookie()
-    }
-  });
-
-  // add a new TODO to the list
-  function addTodo(text) {
-    const newTodo = $('<div class="todo-item"></div>').text(text)
-    $('#ft_list').append(newTodo) // Change prepend to append
-
-    // Click remove TODO
-    newTodo.click(function() {
-      const confirmDelete = confirm('Do you really want to delete this TO DO?')
-      if (confirmDelete) {
-        $(this).remove()
-        saveTodosToCookie() // Save the updated list cookies
-      }
-    })
-  }
-
-  // save TODO to cookies
-  function saveTodosToCookie() {
-    const todoArray = []
-    $('#ft_list .todo-item').each(function() {
-      todoArray.push($(this).text())
+    $('#new-task').click(function() {
+        var task = prompt('Enter a new TO DO:');
+        if (task && $.trim(task) !== "") {
+            addTask(task);
+            saveTasks();
+        }
     });
-    document.cookie = 'todos=' + JSON.stringify(todoArray) + '; path=/'
-  }
 
-  // load TODO from cookies
-  function loadTodosFromCookie() {
-    const cookieString = document.cookie.split('; ').find(row => row.startsWith('todos='))
-    if (cookieString) {
-      const todoArray = JSON.parse(cookieString.split('=')[1])
-      todoArray.forEach(function(todoText) {
-        addTodo(todoText)
-      })
+    function addTask(taskText) {
+        var $taskDiv = $('<div></div>').text(taskText).click(function() {
+            if (confirm('Do you want to remove this TO DO?')) {
+                $(this).remove();
+                saveTasks();
+            }
+        });
+        $('#ft_list').prepend($taskDiv);
     }
-  }
-})
+
+    function saveTasks() {
+        var tasks = [];
+        $('#ft_list > div').each(function() {
+            tasks.push($(this).text());
+        });
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    function loadTasks() {
+        var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        $.each(tasks, function(index, task) {
+            addTask(task);
+        });
+    }
+});
